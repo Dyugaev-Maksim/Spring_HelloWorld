12511cf86447b19a6e58ebbfa964a88b1c38788d
Max.D <veagud@mail.ru>
2023-01-11 21:33:57 +0300

    Кодстайл отредактировал.

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,17 +4,18 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
-    @Bean(name="cat")
+
+    @Bean(name = "cat")
     @Scope("prototype")
-    public Cat getCat(){
-        Cat cat=new Cat();
+    public Cat getCat() {
+        Cat cat = new Cat();
         cat.setMessage("Гав!");
         return cat;
     }
